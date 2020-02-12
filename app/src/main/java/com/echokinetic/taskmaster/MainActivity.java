344967aab817687254dddaf03bc011924b6f65dc
Mattäus Sebastian Isenberg <mattaus.isenberg@hotmail.com>
2020-02-12 11:45:19 -0800

    All tasks for day 2 completed

--- app/src/main/java/com/echokinetic/taskmaster/MainActivity.java
+++ app/src/main/java/com/echokinetic/taskmaster/MainActivity.java
@@ -2,9 +2,14 @@ package com.echokinetic.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +30,42 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, allTasks.class);
             startActivity(intent);
         });
+
+        Button left_Button = findViewById(R.id.left_Button);
+        left_Button.setOnClickListener( (e)-> {
+            Intent intent = new Intent(this, detailPage.class);
+            intent.putExtra("taskTitle", left_Button.getText().toString());
+            startActivity(intent);
+        });
+
+        Button middle_Button = findViewById(R.id.middle_Button);
+        middle_Button.setOnClickListener( (e)-> {
+            Intent intent = new Intent(this, detailPage.class);
+            intent.putExtra("taskTitle", middle_Button.getText().toString());
+            startActivity(intent);
+        });
+
+        Button right_Button = findViewById(R.id.right_Button);
+        right_Button.setOnClickListener( (e)-> {
+            Intent intent = new Intent(this, detailPage.class);
+            intent.putExtra("taskTitle", right_Button.getText().toString());
+            startActivity(intent);
+        });
+
+        ImageButton settings = findViewById(R.id.settingsButton);
+        settings.setOnClickListener( (e)-> {
+            Intent intent = new Intent(this, settings.class);
+           // intent.putExtra("taskTitle", right_Button.getText().toString());
+            startActivity(intent);
+        });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        TextView title = findViewById(R.id.mainTitle);
+        title.setText(pref.getString("user", "Test")+ " Task's");
+    }
 }
---
+++ app/src/main/java/com/echokinetic/taskmaster/settings.java
@@ -0,0 +1,39 @@
+package com.echokinetic.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.function.Predicate;
+
+public class settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = pref.edit();
+
+        Button save_Button = findViewById(R.id.usernameSave);
+        save_Button.setOnClickListener( (e)-> {
+            EditText usernameField = findViewById(R.id.username);
+            String usernameContent = usernameField.getText().toString();
+            editor.putString("user", usernameContent);
+            editor.apply();
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } );
+    }
+
+
+}
